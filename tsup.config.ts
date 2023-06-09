9d0ee15a8ad63893031cc5a7a8bb6c8300be7f05
すずねーう <suzuneu@suzuneu.com>
2023-06-09 03:06:04 +0000

    bug fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,43 @@
+import type { Options } from 'tsup';
+import { defineConfig } from 'tsup';
 import { esbuildPluginVersionInjector } from 'esbuild-plugin-version-injector';
-import { createTsupConfig } from '../../tsup.config.js';
+
+
+function createTsupConfig({
+	entry = ['src/index.ts'],
+	external = [],
+	noExternal = [],
+	platform = 'node',
+	format = ['esm', 'cjs'],
+	target = 'es2022',
+	skipNodeModulesBundle = true,
+	clean = true,
+	shims = true,
+	minify = false,
+	splitting = false,
+	keepNames = true,
+	dts = true,
+	sourcemap = true,
+	esbuildPlugins = [],
+}: Options = {}) {
+	return defineConfig({
+		entry,
+		external,
+		noExternal,
+		platform,
+		format,
+		skipNodeModulesBundle,
+		target,
+		clean,
+		shims,
+		minify,
+		splitting,
+		keepNames,
+		dts,
+		sourcemap,
+		esbuildPlugins,
+	});
+}
 
 export default createTsupConfig({
 	entry: ['src/index.ts', 'src/strategies/*.ts'],
